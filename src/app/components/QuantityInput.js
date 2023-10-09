ed44b781fc0cc60c3ebb55bb0d78a953541cdfa6
Giselle <milanoregina8@gmail.com>
2023-10-09 17:07:04 -0300

    QuantityInput value attribute fixed I changed default value attribute to value attribute. I removed logic else in handleQuantityChange method

--- src/app/components/QuantityInput.js
+++ src/app/components/QuantityInput.js
@@ -4,15 +4,13 @@ export default function QuantityInput(props) {
   const handleQuantityChange = (event) => {
     if (/^[0-9]*$/.test(event.target.value)) {
       props.onQuantityChange(Number(event.target.value));
-    } else {
-      props.onQuantityChange(0);
     }
   };
 
   return (
     <input
       size={2}
-      defaultValue={props.quantity}
+      value={props.quantity}
       onChange={handleQuantityChange}
       className="border border-neutral-600 text-center text-neutral-600 text-xs"
     />
